9bb1eeec3797c9ca1fb35b31cc0e4cd7598eae81
@jhenanne <jensa@Jhenanne-APH300>
2023-09-25 16:52:40 +0800

    Second Commit

--- src/activityone/ActivityOne.java
+++ src/activityone/ActivityOne.java
@@ -16,6 +16,7 @@ public class ActivityOne {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello World");
+        System.out.printlh("Modified code");
     }
     
 }

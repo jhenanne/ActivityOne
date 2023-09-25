baaf178a65f5c24f68f424f10cc32fa9d6244fde
@jhenanne <jensa@Jhenanne-APH300>
2023-09-25 18:20:00 +0800

    Fourth commit

--- src/activityone/ActivityOne.java
+++ src/activityone/ActivityOne.java
@@ -16,6 +16,7 @@ public class ActivityOne {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello World");
+        System.out.println("Sample for GIT commit and push");
     }
     
 }
